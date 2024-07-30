74c3112ecb8b3d4b38df2b804dd27c3801eb9dda
riho-riho <99725441+riho-riho@users.noreply.github.com>
2024-07-30 17:30:34 +0900

    kadai_007の追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        $item_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+        print_r($item_data);
+
+        ?>
+    </p>
+</body>
+</html>
